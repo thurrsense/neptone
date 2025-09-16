bf4be22b5ccc184e36a3b9286072aba3e2f1785e
AKhasanov <arthur.khasanov20@gmail.com>
2025-09-16 09:47:24 +0200

    CHG: profile + home + navbar

--- neptone/users/views.py
+++ neptone/users/views.py
@@ -209,7 +209,7 @@ def register(request):
         if form.is_valid():
             user = form.save()
             login(request, user)           # залогиним сразу
-            return redirect('profile')     # и ведём в профиль
+            return redirect('my_profile')   # и ведём в профиль
     else:
         form = RegistrationForm()
     return render(request, 'users/register.html', {'form': form})
